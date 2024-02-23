dc42353178ef54100e6e2eda9c42cc20f3f0ad65
RafaelReis <86300917+rfael5@users.noreply.github.com>
2024-02-23 09:27:14 -0300

    att

--- totalProdutosComposicao.py
+++ totalProdutosComposicao.py
@@ -1,11 +1,7 @@
 from sqlalchemy import create_engine
 import pandas as pd
-import requests
 import json
 from datetime import datetime
-
-import openpyxl
-from openpyxl.styles import PatternFill
 
 conexao = (
     "mssql+pyodbc:///?odbc_connect=" + 
@@ -19,7 +15,7 @@ engine = create_engine(conexao, pool_pre_ping=True)
 
 queryProdutosComposicao =  """
 select 
-	 e.PK_DOCTOPED as idEvento, e.NOME as nomeEvento, e.DTEVENTO as dataEvento, p.PK_MOVTOPED as idMovtoped, c .IDX_LINHA as linha, p.DESCRICAO as nomeProdutoAcabado, ca.RENDIMENTO as rendimento, p.UNIDADE as unidadeAcabado, a.RDX_PRODUTO as idProdutoAcabado, c.DESCRICAO as nomeProdutoComposicao, c.PK_PRODUTO as idProdutoComposicao, a.QUANTIDADE as qtdProdutoComposicao, a.UN as unidadeComposicao, p.L_QUANTIDADE as qtdProdutoEvento 
+	 e.PK_DOCTOPED as idEvento, e.NOME as nomeEvento, e.DTEVENTO as dataEvento, p.PK_MOVTOPED as idMovtoped, ca.IDX_LINHA as linha, p.DESCRICAO as nomeProdutoAcabado, ca.RENDIMENTO as rendimento, p.UNIDADE as unidadeAcabado, a.RDX_PRODUTO as idProdutoAcabado, c.DESCRICAO as nomeProdutoComposicao, c.PK_PRODUTO as idProdutoComposicao, a.QUANTIDADE as qtdProdutoComposicao, a.UN as unidadeComposicao, p.L_QUANTIDADE as qtdProdutoEvento
 from TPAPRODCOMPOSICAO as a 
 	inner join TPAPRODUTO as c on a.IDX_PRODUTO = c.PK_PRODUTO
 	inner join TPAMOVTOPED as p on a.RDX_PRODUTO = p.IDX_PRODUTO
@@ -82,6 +78,30 @@ where e.DTPREVISAO between '20240227' and '20240228'
 group by c.PK_PRODUTO, c.DESCRICAO, a.UN
 """
 
+queryAjustes = """
+select A.IDX_MOVTOPED AS idMovtoped, V.IDX_PRODUTO AS idProduto, V.DESCRICAO AS nomeProduto, A.QUANTIDADE AS ajuste, A.PRECO AS precoAjuste from TPAAJUSTEPEDITEM AS A 
+	inner join TPAMOVTOPED AS V ON A.IDX_MOVTOPED = V.PK_MOVTOPED
+	inner join TPADOCTOPED AS E ON V.RDX_DOCTOPED = E.PK_DOCTOPED
+	inner join TPAPRODUTO AS P ON V.IDX_PRODUTO = P.PK_PRODUTO
+where e.TPDOCTO = 'EC' 
+	and E.DTPREVISAO between '20240227' and '20240228'
+	and E.SITUACAO = 'Z'
+    and P.OPSUPRIMENTOMP = 'S'
+or e.TPDOCTO = 'EC' 
+	and E.DTPREVISAO between '20240227' and '20240228'
+	and E.SITUACAO = 'B'
+    and P.OPSUPRIMENTOMP = 'S'
+or e.TPDOCTO = 'OR' 
+	and E.DTPREVISAO between '20240227' and '20240228'
+	and E.SITUACAO = 'V'
+    and P.OPSUPRIMENTOMP = 'S'
+or e.TPDOCTO = 'OR' 
+	and E.DTPREVISAO between '20240227' and '20240228'
+	and E.SITUACAO = 'B'
+    and P.OPSUPRIMENTOMP = 'S'
+ORDER BY V.DESCRICAO
+"""
+
 def receberDados(query):
     response = pd.read_sql_query(query, engine)
     resultadosJson = response.to_json(orient='records')
@@ -89,7 +109,7 @@ def receberDados(query):
     return dadosDesserializados
 
 produtosComposicao = receberDados(queryProdutosComposicao)
-totalProdutosEvento = receberDados(queryProdutosAcabados)
+ajustes = receberDados(queryAjustes)
 
 qtdTotalProducao = []
 
@@ -98,6 +118,11 @@ def verificarAjustes(evento):
     resultado = receberDados(queryAjustes)
     if len(resultado) > 0:
         evento['qtdProdutoEvento'] = evento['qtdProdutoEvento'] + resultado[0]['qtdAlteracao']
+
+def adicionarAjustes(evento):
+    for a in ajustes:
+        if a['idMovtoped'] == evento['idMovtoped']:
+            evento['qtdProdutoEvento'] = evento['qtdProdutoEvento'] + a['ajuste']
 
 
 def recuperarHoraAtual():
@@ -109,41 +134,34 @@ def recuperarHoraAtual():
 
 def calcularQtdProducao():
     for e in produtosComposicao:
-        verificarAjustes(e)
         if e['unidadeAcabado'] == 'PP':
-            #total = e["qtdProdutoComposicao"] * (e["qtdProdutoEvento"] / 10)
-            #CALCULO PARA RENDIMENTO: [(qtdEvento / 10) * qtdProdutoComposicao] / rendimento
             total = (e["qtdProdutoEvento"] / 10) * e["qtdProdutoComposicao"]
             rendimento = total / e["rendimento"]
             e["totalProducao"] = rendimento
-        elif e['unidadeAcabado'] == 'UN':
-            rendimento = (e['qtdProdutoComposicao'] * e['qtdProdutoEvento']) / e['rendimento']
-            e['totalProducao'] = rendimento
+        elif e['unidadeAcabado'] == 'UD':
+            total = (e['qtdProdutoEvento'] / 100) * e['qtdProdutoComposicao']
+            e['totalProducao'] = total
+        elif e['unidadeAcabado'] == 'UM':
+            total = (e['qtdProdutoEvento'] / 10) * e['qtdProdutoComposicao']
+            e['totalProducao'] = total
         else:
             total = e["qtdProdutoComposicao"] * e["qtdProdutoEvento"]
             e["totalProducao"] = total
 
-
 calcularQtdProducao()
+
+
+def aplicarAjustes():
+    for p in produtosComposicao:
+        adicionarAjustes(p)
+
+aplicarAjustes()
 
 def gerarArquivoExcel(tipoArquivo, listaProdutos):
     caminho_arquivo_excel = f'C:\\Users\\serverteste\\Desktop\\teste2\\arquivos\\{tipoArquivo}--{recuperarHoraAtual()}.xlsx'
     formatoTabela = pd.DataFrame(listaProdutos)
     formatoTabela.to_excel(caminho_arquivo_excel)
     print("ARQUIVO EXCEL GERADO")
-
-
-def formatarExcel(caminhoArquivo):
-    workbook = openpyxl.load_workbook('caminhoArquivo')
-    
-    sheet = workbook.active
-
-
-def calcularRendimento(unidadeAcabado, quantidade):
-    if unidadeAcabado == 'PP':
-        return quantidade / 10
-    else:
-        return quantidade
 
 somaProdutosEventos = []
 
@@ -175,61 +193,38 @@ def agruparLinhas(produto):
         
     for x in range(1, 7):
         if produto['linha'] == f'S{x}':
-            produto['linha'] = 'Sal'
+            return 'Sal'
     
     for x in range(1, 7):
         if produto['linha'] == f'M-{x}':
-            produto['linha'] = 'Doces'
+            return 'Doces'
     
     for x in range(1, 4):
         if produto['linha'] == f'C-{x}' or produto['linha'] == 'Doce Geral':
-            produto['linha'] = 'Doces'
+            return 'Doces'
     
     if produto['linha'] == 'S7' or produto['linha'] == 'S8':
-        produto['linha'] = 'Refeições'           
+        return 'Refeições'           
 
-
-#e.PK_DOCTOPED as idEvento, e.NOME as nomeEvento, e.DTEVENTO as dataEvento, p.PK_MOVTOPED as idMovtoped, c.IDX_LINHA as linha, p.DESCRICAO as nomeProdutoAcabado, ca.RENDIMENTO as rendimento, p.UNIDADE as unidadeAcabado, a.RDX_PRODUTO as idProdutoAcabado, c.DESCRICAO as nomeProdutoComposicao, c.PK_PRODUTO as idProdutoComposicao, a.QUANTIDADE as qtdProdutoComposicao, a.UN as unidadeComposicao, p.L_QUANTIDADE as qtdProdutoEvento 
 
 def somarProdutosEvento():
+        
     dfComposicao = pd.DataFrame(produtosComposicao)
-    
-    #dfComposicao = dfComposicao.groupby(['idEvento', 'nomeEvento', 'dataEvento', 'idMovtoped', 'linha', 'nomeProdutoAcabado', 'rendimento', 'unidadeAcabado', 'idProdutoAcabado', 'nomeProdutoComposicao', 'idProdutoComposicao', 'qtdProdutoComposicao', 'unidadeComposicao', 'qtdProdutoEvento' ]).reset_index()
+
     dfComposicao.drop_duplicates(inplace=True)
-    novaLista = []
 
     dfComposicao['unidade'] = dfComposicao['unidadeComposicao'].apply(alterarStringUnidade)
     dfComposicao['totalProducao'] = dfComposicao.apply(converterKg, axis=1)
+    dfComposicao['linha'] = dfComposicao.apply(agruparLinhas, axis=1)
     
-    result = dfComposicao.groupby(['idProdutoComposicao', 'nomeProdutoComposicao', 'unidade', 'linha', 'idProdutoAcabado', 'nomeProdutoAcabado'])[['totalProducao']].sum().reset_index()
+    result = dfComposicao.groupby(['idProdutoComposicao', 'nomeProdutoComposicao', 'unidade', 'linha'])[['totalProducao']].sum().reset_index()
 
     result['unidade'] = result['unidade'].apply(mudarUnidade)
     
     resultJson = result.to_json(orient='records')
     dadosDesserializados = json.loads(resultJson)
-
-    for p in dadosDesserializados:
-        for pa in totalProdutosEvento:
-            if p['idProdutoAcabado'] == pa['idProduto'] and 'Produtos acabados' in pa['negocio']:
-                p['linha'] = pa['linha']
-                if p not in novaLista:
-                    novaLista.append(p)
     
-    for p in novaLista:
-        agruparLinhas(p)
-    
-    # for p in produtosComposicao:
-    #     if 'Lombo de Bacalhau Saithe Congelado' in p['nomeProdutoComposicao']:
-    #         print(p)
-    
-    dfNovaLista = pd.DataFrame(novaLista)
-    resultFinal = dfNovaLista.groupby(['idProdutoComposicao', 'nomeProdutoComposicao', 'unidade', 'linha'])[['totalProducao']].sum().reset_index()
-    
-    rFinalJson = resultFinal.to_json(orient='records')
-    rFinalDesserializados = json.loads(rFinalJson)
-    
-    #separarProdutosEvento(rFinalDesserializados)
-    
+    separarProdutosEvento(dadosDesserializados)
     
 
 def filtrarListas(tipoFiltro, listaCompleta):
@@ -249,7 +244,3 @@ def separarProdutosEvento(listaProdutos):
 
 somarProdutosEvento()
 
-
-for p in produtosComposicao:
-        if 'Alho Porró unid' in p['nomeProdutoComposicao']:
-            print(p)
